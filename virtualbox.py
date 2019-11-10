211a9cb44fbf938ed42f2eda79a7c179010bfe4c
nedx86 <57058172+nedx86@users.noreply.github.com>
2019-11-11 00:47:04 +0200

    Update virtualbox.py

--- virtualbox.py
+++ virtualbox.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 import getpass
@@ -11,45 +11,49 @@ import os
 def action(vm_name, vm_action):
     cmd = ""
 
-    #vm_action = "state"
-    #vm_name = "none"
-
-
     if vm_action == "start":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage startvm '{}' --type headless\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage startvm '{}' --type headless\"".format(vm_name),"r")
 
     if vm_action == "show":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage startvm '{}' --type separate\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage startvm '{}' --type separate\"".format(vm_name),"r")
 
     if vm_action == "stop":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' acpipowerbutton\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' acpipowerbutton\"".format(vm_name),"r")
 
     if vm_action == "shutdown":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' poweroff\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' poweroff\"".format(vm_name),"r")
 
     if vm_action == "reboot":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' reset\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' reset\"".format(vm_name),"r")
 
     if vm_action == "save":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' savestate\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' savestate\"".format(vm_name),"r")
 
     if vm_action == "pause":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' pause\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' pause\"".format(vm_name),"r")
 
     if vm_action == "resume":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage controlvm '{}' resume\"".format(vm_name),"r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage controlvm '{}' resume\"".format(vm_name),"r")
 
     if vm_action == "state":
-        cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage list vms --long | grep -e 'Name:' -e 'State:'\"","r")
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage list vms --long | grep -e 'Name:' -e 'State:'\"","r")
 
-    
-    #print (cmd.readlines())
-
-    while 1:
-        line = cmd.readline()
-        if not line: break
-        print (line.encode("UTF-8"))
-        #result = result + line + "\n"
+    if (vm_action == "saveall"):
+        vms = []
+        cmd = os.popen("sudo -H -u $USER bash -c \"VBoxManage list runningvms | sed -r 's/.*\{(.*)\}/\\1/'\"","r")
+        while 1:
+            line = cmd.readline()
+            if not line: break
+            vms.append(line.replace('\n', ''))
+            print (vms)
+        for vm in vms:
+            action(vm, "save")
+            print(vm)
+        while 1:
+            line = cmd.readline()
+            if not line: break
+            print (line.encode("UTF-8"))
+        print("\n")
 
 cmd = ""
 parser = argparse.ArgumentParser(description='VirtualBox Client')
@@ -59,20 +63,4 @@ args = parser.parse_args()
 vm_name = args.name
 vm_action = args.action
 
-#print ("NAME: {}", vm_name)
-#print ("ACTION: {}", vm_action)
-
-if (vm_action == "saveall"):
-    vms = []
-    cmd = os.popen("sudo -H -u ad bash -c \"VBoxManage list runningvms | sed -r 's/.*\{(.*)\}/\\1/'\"","r")
-    while 1:
-        line = cmd.readline()
-        if not line: break
-        vms.append(line.replace('\n', ''))
-        print (vms)
-        #result = result + line + "\n"
-    for vm in vms:
-        action(vm, "save")
-        print(vm)
-else:
-    action(vm_name, vm_action)
+action(vm_name, vm_action)
